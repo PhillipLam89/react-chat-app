18730d226340b8d00c6a50e5df47ccc83b63b306
Phillip Lam <plam89@ucla.edu>
2021-04-18 01:23:27 -0700

    users now able to see each others messages in the chat room

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -11,7 +11,7 @@ const Input = ({message, setMessage, sendMessage}) => (
       placeholder="Type a message..."
       value={message}
       onChange={e => setMessage(e.target.value)}
-      onKeyPress={e => e.key === 'Enter' ? sendMessage(e) : null}
+      onKeyPress={e => e.key === 'Enter' && sendMessage(e)} //allows for user to press enter to submit as well
     />
     <button className="sendButton" onClick={e => sendMessage(e)}>Send</button>
   </form>
